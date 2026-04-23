f3621c08e91e108e430151d35bace5ec6a7810dc
Kai <7469816+kaimeraq@users.noreply.github.com>
2026-04-23 15:47:47 -0500

    K - Fixed min/max conflict with Windows header

--- src/Platform/Windows/OSWindows.h
+++ src/Platform/Windows/OSWindows.h
@@ -9,6 +9,15 @@
 #endif
 
 #ifdef PLATFORM_WINDOWS
+
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
 #include <windows.h>
 #endif
 
