fc1bddc53cfaaa8c0471a95feb177501df3adbe6
Kai <7469816+kaimeraq@users.noreply.github.com>
2026-04-23 15:47:13 -0500

    Leslie - Added assert to make sure that the project was made in CPP23

--- src/Platform/PlatformGeneric.h
+++ src/Platform/PlatformGeneric.h
@@ -1,5 +1,8 @@
 #pragma once
 
+// C++ 23 is required for this project
+static_assert(__cplusplus >= 202302L);
+
 typedef char        ANSICHAR;
 typedef wchar_t	    WIDECHAR;
 typedef WIDECHAR    GENCHAR;
