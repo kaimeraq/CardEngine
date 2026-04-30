5bef46b786c53e99f596408fc9e709600d7cdc32
Kai <7469816+kaimeraq@users.noreply.github.com>
2026-04-29 19:38:15 -0500

    K - Moved chrono functions into translation unit instead of inline

--- src/Core/Engine.cpp
+++ src/Core/Engine.cpp
@@ -1,1 +1,12 @@
 #include "Engine.h"
+
+std::time_t GetCurrentTimeSeconds()
+{
+    auto now = std::chrono::system_clock::now();
+    return std::chrono::system_clock::to_time_t(now);
+}
+
+std::chrono::system_clock::time_point GetCurrentTimePoint()
+{
+    return std::chrono::system_clock::now();
+}
--- src/Core/Engine.h
+++ src/Core/Engine.h
@@ -3,13 +3,5 @@
 #include <chrono>
 #include <ctime>
 
-static std::time_t GetCurrentTimeSeconds()
-{
-    auto now = std::chrono::system_clock::now();
-    return std::chrono::system_clock::to_time_t(now);
-}
-
-static std::chrono::system_clock::time_point GetCurrentTimePoint()
-{
-	return std::chrono::system_clock::now();
-}
+std::time_t GetCurrentTimeSeconds();
+std::chrono::system_clock::time_point GetCurrentTimePoint();
