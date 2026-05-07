6647e69de6012761dd91dd0ba8340b03b283be31
Kai <7469816+kaimeraq@users.noreply.github.com>
2026-05-07 13:53:40 -0500

    K - Child renderers can now have their components accessed dynamically

--- src/Renderer/Renderer.h
+++ src/Renderer/Renderer.h
@@ -6,6 +6,25 @@ protected:
     virtual void Flush() {}
     virtual void Clear() {}
 
+    static void SetInstance(Renderer* instance) 
+    {
+        s_instance = instance;
+    }
+
 public:
+    static Renderer& Get() 
+    {
+        return *s_instance;
+    }
+
+    template<typename T>
+    T* As()
+    {
+        return dynamic_cast<T*>(this);
+    }
+
     virtual ~Renderer() = default;
+
+private:
+    static inline Renderer* s_instance = nullptr;
 };
